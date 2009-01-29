0e22538c776d2b0523eb24ca5f13f2411662ab75
chris.read <chris.read@59ab4f38-e3ec-11dd-a8eb-ddf07f3c1838>
2009-01-29 21:27:05 +0000

    Clean up the controller. Found two cases we need to write tests for

--- controller/environments.rb
+++ controller/environments.rb
@@ -1,5 +1,6 @@
 
 class EnvironmentsController < Ramaze::Controller
+    map('/environments')
 
     def index(env = nil, app = nil, key = nil)
         # Getting...
@@ -7,49 +8,15 @@ class EnvironmentsController < Ramaze::Controller
             # List all environments
             if env.nil?
                 return Environment
-
-
             # List all apps in specified environment
             elsif app.nil?
-                myenv = Environment[:name => env]
-                if myenv.nil?
-                    response.status = 404
-                else
-                    #apps = DB[:apps].where(:environment => myenv[:id])
-                    apps = DB[:apps]
-                    return apps.all
-                end
-
-
+                listApps(env)
             # List keys and values for app in environment
             elsif key.nil?
-                myenv = Environment[:name => env]
-                if myenv.nil? # Env does not exist
-                    response.status = 404
-                else
-                    apps = App[:environment => myenv[:id]]
-                    if apps.nil?
-                        response.status = 404
-                    else
-                        return apps.all
-                    end
-                end
-
-
+                listKeys(env, app)
             # We're getting value for specific key
             else 
-                values = Value[:key => key, :app => App[:name => app][:id], :environment => Environment[:name => env][:id]]
-                if values.nil? 
-                    # Not in the specified env, is it in default?
-                    values = Value[:key => key, :app => App[:name => app][:id], :environment => Environment[:name => "default"][:id]]
-                    if values.nil?
-                        response.status = 404
-                    else
-                        return values[:value]
-                    end
-                else
-                    return values[:value]
-                end
+                getValue(env, app, key)
             end
 
 
@@ -58,42 +25,99 @@ class EnvironmentsController < Ramaze::Controller
             # Undefined
             if env.nil?
                 response.status = 400
-
-
             # You're putting an env
             elsif app.nil?
-                begin
-                    Environment.create(:name => env)
-                    response.status = 201
-                rescue
-                    response.status = 403
-                end
-
-
+                createEnv(env)
             # You're putting an app
             elsif key.nil?
-                begin
-                    App.create(:name => app, :environment => Environment[:name => env][:id])
-                    response.status = 201
-                rescue
-                    response.status = 403
-                end
-
-
+                createApp(env, app)
             # You're putting a value to a key
             else             
-                value = request.body.read
-                myvalue = Value[:app => App[:name => app][:id], :environment => Environment[:name => env][:id], :key => key]
-                # New one, let's create
-                if myvalue.nil?
-                    Value.create(:key => key, :value => value, :app => App[:name => app][:id], :environment => Environment[:name => env][:id])
-                    response.status = 201
-                # We're updating the config
-                else             
-                    myvalue.update(:value => value)
-                    response.status = 200
-                end
+                setValue(env, app, key)
             end
+        end
+    end
+
+
+    private
+
+    def listApps(env)
+        # List all apps in specified environment
+        myenv = Environment[:name => env]
+        if myenv.nil?
+            response.status = 404
+        else
+            # TODO: Create a test that shows we need the line below
+            #apps = DB[:apps].where(:environment => myenv[:id])
+            # TODO: Clean this up
+            apps = DB[:apps]
+            return apps.all
+        end
+    end
+
+    
+    def listKeys(env, app)
+        # List keys and values for app in environment
+        myenv = Environment[:name => env]
+        if myenv.nil? # Env does not exist
+            response.status = 404
+        else
+            # TODO: Write a test that show the line below is needed
+            #apps = App[:environment => myenv[:id], :environment => myenv[:id]]
+            apps = App[:environment => myenv[:id]]
+            if apps.nil?
+                response.status = 404
+            else
+                return apps.all
+            end
+        end
+    end
+
+
+    def getValue(env, app, key)
+        values = Value[:key => key, :app => App[:name => app][:id], :environment => Environment[:name => env][:id]]
+        if values.nil? 
+            # Not in the specified env, is it in default?
+            values = Value[:key => key, :app => App[:name => app][:id], :environment => Environment[:name => "default"][:id]]
+            if values.nil?
+                response.status = 404
+            else
+                return values[:value]
+            end
+        else
+            return values[:value]
+        end
+    end
+
+    def createEnv(env)
+        begin
+            Environment.create(:name => env)
+            response.status = 201
+        rescue
+            response.status = 403
+        end
+    end
+
+    def createApp(env, app)
+        begin
+            App.create(:name => app, :environment => Environment[:name => env][:id])
+            response.status = 201
+        rescue
+            response.status = 403
+        end
+    end
+
+    def setValue(env, app, key)
+        value = request.body.read
+        myvalue = Value[:app => App[:name => app][:id], :environment => Environment[:name => env][:id], :key => key]
+        # New one, let's create
+        if myvalue.nil?
+            Value.create(:key => key, :value => value, :app => App[:name => app][:id], :environment => Environment[:name => env][:id])
+            response.status = 201
+        # We're updating the config
+        else             
+            myvalue.update(:value => value)
+            response.status = 200
         end
     end
 end
