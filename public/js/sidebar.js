bad34083cefeadaf4578912d722d51625b5b98df
chris.read <chris.read@59ab4f38-e3ec-11dd-a8eb-ddf07f3c1838>
2009-02-11 10:50:47 +0000

    Fix cascading event bug, fix expand/collapse

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -8,41 +8,13 @@ var EscSidebar = function() {
         makeCollapsible : function(target, subHead) {
             //By Default put the Menu in collapsed state
             $('.children').parent().children('ul').slideUp('fast');
-
-            //Expand or Contract one particular Nested ul
-            $('img', subHead).addClass('clickable').click(function() {
-                var toggleSrc = $(this).attr('src');
-                if ( toggleSrc == toggleMinus ) {
-                    $(this).attr('src', togglePlus).parent().children('ul').slideUp('fast');
-                } else{
-                    $(this).attr('src', toggleMinus).parent().children('ul').slideDown('fast');
-                };
-            });
-	
-            // Click on an app to get stuff in the content pane
-            $(target + ' li li').click(function(appclick) {
-                var thisEnv = $(this).parents("li:first").attr("id").replace('_env', '');
-                var thisApp = $(this).attr("id").replace('_app', '');
-                var url = "/environments/" + thisEnv + "/" + thisApp;
-                EscEditor.editPropertiesFor(thisEnv, thisApp);
-            });
-        },
-
-        contract : function(target) {
-            target.attr('src', toggleMinus).children('ul').slideUp('fast');
-            $('img', target).attr('src', togglePlus);
-        },
-
-        expand : function(target) {
-            target.children('ul').slideDown('fast');
-            $('img', target).attr('src', toggleMinus);
         },
 
         getListofEnvsAndApps : function(target) {
             $.getJSON("/environments", function(envData) {
                 var envList = "<ul>";
                 $.each(envData, function(i, thisEnv) {
-                    envList += ('<li id="' + thisEnv + '_env"><img src="' + togglePlus + '" alt="collapse this section" > ' + thisEnv);
+                    envList += ('<li id="' + thisEnv + '_env"><img src="' + toggleMinus + '" alt="collapse this section" class="expander" class="clickable"> ' + thisEnv);
                     var url = "/environments/" + thisEnv;
                     $.getJSON(url, function(appData) {
                         appList = '<ul class="children"';
@@ -95,21 +67,41 @@ var EscSidebar = function() {
 
 $(document).ready(function() {
     // Get nested list of environments/apps
-    $('#show_env').click(function() {
-        var subHead = $('.children').parent();
-        EscSidebar.makeCollapsible('#sidebar', subHead);
-    })
+    //$('#show_env').click(function() {
+    //    var subHead = $('.children').parent();
+    //    EscSidebar.makeCollapsible('#sidebar', subHead);
+    //})
 
     //Expand All Code
     $('.expand').live("click", function() {
-        $(this).children('ul').slideDown('fast');
-        $('img', $(this)).attr('src', toggleMinus);
+        $('.children').slideDown('fast');
+        $('img', $('.children').parent()).attr('src', toggleMinus);
     });
 
     //Contract All Code
     $('.contract').live("click", function() {
-        $(this).attr('src', toggleMinus).children('ul').slideUp('fast');
-        $('img', $(this)).attr('src', togglePlus);
+        $('.children').parent().attr('src', toggleMinus).children('ul').slideUp('fast');
+        $('img', $('.children').parent()).attr('src', togglePlus);
+    });
+
+    // Click on an app to get stuff in the content pane
+    $('#sidebar li li').live("click", function() {
+        var thisEnv = $(this).parents("li:first").attr("id").replace('_env', '');
+        var thisApp = $(this).attr("id").replace('_app', '');
+        EscEditor.editPropertiesFor(thisEnv, thisApp);
+    });
+
+    var toggleMinus = '/images/minus.jpg';
+    var togglePlus = '/images/plus.jpg';
+
+    //Expand or Contract one particular Nested ul
+    $('.expander').live("click", function() {
+        var toggleSrc = $(this).attr('src');
+        if ( toggleSrc == toggleMinus ) {
+            $(this).attr('src', togglePlus).parent().children('ul').slideUp('fast');
+        } else{
+            $(this).attr('src', toggleMinus).parent().children('ul').slideDown('fast');
+        };
     });
 
     EscSidebar.getListofEnvsAndApps('#sidebar');
