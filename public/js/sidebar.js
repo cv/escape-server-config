c52e1eb1778776dd15ae5a5f3df8b394cc85c755
chris.read <chris.read@59ab4f38-e3ec-11dd-a8eb-ddf07f3c1838>
2009-02-15 21:00:41 +0000

    Clean up some javascript, fix issue #1 by only loading application list for environment on demand.

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -11,33 +11,34 @@ var EscSidebar = function() {
         newEnvLabel : newEnvLabel,
         newAppLabel : newAppLabel,
 
-        getListofEnvsAndApps : function() {
+        loadApplicationsForEnvironment : function(envId, envName) {
+            var url = "/environments/" + envName;
+            $.getJSON(url, function(appData) {
+                var appList = '<ul class="application_list" style="display: none;"';
+                $.each(appData, function(appId, thisApp) {
+                    appList += "<li class='application'>" + thisApp + "</li>";
+                });
+                appList += '<li><form id="' + envName + '_new_app_form" class="new_app_form" action="javascript:void(0);">' + newAppLabel + '<input type="text" id="new_app_name"/></form></li>';
+                appList += "</ul>";
+                var envObj = $('#sidebar .environment:eq(' + envId + ')');
+                envObj.append(appList);
+                envObj.children('ul').slideDown('fast');
+                $('#sidebar' + " .new_app_form").submit(EscSidebar.createNewApp);
+            });
+        },
+
+        loadEnvironments : function() {
             $.getJSON("/environments", function(envData) {
-                var envList = "<ul class='env_list'>";
-                $.each(envData, function(envId, thisEnv) {
+                var envList = "<ul class='environment_list'>";
+                $.each(envData, function(envId, envName) {
                     var toggleState;
-                    if ($('#sidebar').data(thisEnv + '_expanded')) {
+                    if ($('#sidebar').data(envName + '_expanded')) {
                         toggleState = toggleMinus;
+                        EscSidebar.loadApplicationsForEnvironment(envId, envName);
                     } else {
                         toggleState = togglePlus;
                     };
-                    envList += ('<li class="environment"><img src="' + toggleState + '" alt="collapse this section" class="expander" class="clickable"><span>' + thisEnv + '</span>');
-                    var url = "/environments/" + thisEnv;
-                    $.getJSON(url, function(appData) {
-                        var myEnv = thisEnv;
-                        appList = '<ul class="app_list"';
-                        $.each(appData, function(appId, thisApp) {
-                            appList += "<li class='app'>" + thisApp + "</li>";
-                        });
-                        appList += '<li><form id="' + myEnv + '_new_app_form" class="new_app_form" action="javascript:void(0);">' + newAppLabel + '<input type="text" id="new_app_name"/></form></li>';
-                        appList += "</ul>";
-                        var envObj = $('#sidebar .environment:eq(' + envId + ')');
-                        envObj.append(appList);
-                        if (! $('#sidebar').data(myEnv + '_expanded')) {
-                            envObj.children('ul').slideUp('fast');
-                        } 
-                        $('#sidebar' + " .new_app_form").submit(EscSidebar.createNewApp);
-                    });
+                    envList += ('<li class="environment"><img src="' + toggleState + '" class="expander_img" class="clickable"><span>' + envName + '</span>');
                     envList += ('</li>');
                 });
                 envList += '<li><form id="new_env_form" action="javascript:void(0);">' + newEnvLabel + '<input type="text" id="new_env_name" name="new_env_name"/></form></li>';
@@ -70,7 +71,7 @@ var EscSidebar = function() {
                     url: "/environments/" + newName,
                     data: {},
                     success: function(data, textStatus) {
-                        EscSidebar.getListofEnvsAndApps();
+                        EscSidebar.loadEnvironments();
                     },
                     error: function(XMLHttpRequest, textStatus, errorThrown) {
                         alert("Error creating new environment '" + newName +"': " + XMLHttpRequest.responseText);
@@ -91,7 +92,7 @@ var EscSidebar = function() {
                     url: "/environments/" + envName + "/" + newName,
                     data: {},
                     success: function(data, textStatus) {
-                        EscSidebar.getListofEnvsAndApps();
+                        EscSidebar.loadEnvironments();
                     },
                     error: function(XMLHttpRequest, textStatus, errorThrown) {
                         alert("Error creating new application '" + newName +"': " + XMLHttpRequest.responseText);
@@ -108,17 +109,17 @@ var EscSidebar = function() {
 
 $(document).ready(function() {
     $('#refresh_env').click(function() {
-        EscSidebar.getListofEnvsAndApps();
+        EscSidebar.loadEnvironments();
     })
 
     //Expand All Code
     $('.expand').live("click", function() {
-        $('#sidebar > ul > li > ul').slideDown('fast');
         $('img', $('#sidebar > ul > li')).attr('src', EscSidebar.toggleMinus);
         // Loop through all envs, set collapsed = true
-        $.each($('#sidebar > ul > li'), function(i, item) { 
-            var myEnv = $(item + " span").text();
-            $('#sidebar').data(myEnv + '_expanded', true);
+        $.each($('#sidebar > ul > li > span'), function(id, span) { 
+            var name = $(span).text();
+            $('#sidebar').data(name + '_expanded', true);
+            EscSidebar.loadApplicationsForEnvironment(id, name);
         });
     });
 
@@ -127,9 +128,10 @@ $(document).ready(function() {
         $('#sidebar > ul > li > ul').slideUp('fast');
         $('img', $('#sidebar > ul > li')).attr('src', EscSidebar.togglePlus);
         // Loop through all envs, set collapsed = true
-        $.each($('#sidebar > ul > li'), function(i, item) { 
-            var myEnv = $(item + " span").text();
+        $.each($('#sidebar > ul > li > span'), function(id, span) { 
+            var myEnv = $(span).text();
             $('#sidebar').data(myEnv + '_expanded', false);
+            $(this).siblings('ul').empty();
         });
     });
 
@@ -149,20 +151,27 @@ $(document).ready(function() {
     });
 
     //Expand or Contract one particular Nested ul
-    $('.expander').live("click", function() {
+    $('.expander_img').live("click", function() {
         var toggleSrc = $(this).attr('src');
         var thisEnv = $(this).siblings("span").text();
 
         if ( toggleSrc == EscSidebar.toggleMinus ) {
             $('#sidebar').data(thisEnv + '_expanded', false);
-            $(this).attr('src', EscSidebar.togglePlus).parent().children('ul').slideUp('fast');
+            $(this).attr('src', EscSidebar.togglePlus).siblings('ul').slideUp('fast');
+            $(this).siblings('ul').empty();
         } else{
             $('#sidebar').data(thisEnv + '_expanded', true);
-            $(this).attr('src', EscSidebar.toggleMinus).parent().children('ul').slideDown('fast');
+            $(this).attr('src', EscSidebar.toggleMinus).siblings('ul').slideDown('fast');
+            $.each($('.environment > span'), function(id, span) {
+                var name = $(span).text();
+                if (name == thisEnv) {
+                    EscSidebar.loadApplicationsForEnvironment(id, name);
+                };
+            });
         };
     });
 
-    EscSidebar.getListofEnvsAndApps();
+    EscSidebar.loadEnvironments();
     $('#new_key').hide();
     $('#editor').hide();
 });
