52cec5cdee4eca3181790552c4461478b687833d
chris.read <chris.read@59ab4f38-e3ec-11dd-a8eb-ddf07f3c1838>
2009-02-16 15:05:30 +0000

    Get the release script to generate a changelog for us

--- release.sh
+++ release.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 VERSION=0.2
+PREV_REV=119
+
 REV=`svn info | awk '/^Revision: / { print $2 }'`
 URL=`svn info | awk '/^URL: / { print $2 }'`
 
 mkdir -p dist
-cd dist
-svn export -r ${REV} ${URL} escape-${VERSION}-${REV}
-zip -r -9 escape-${VERSION}-${REV}.zip escape-${VERSION}-${REV}/*
+svn export -r ${REV} ${URL} dist/escape-${VERSION}-${REV}
+svn2cl.sh --break-before-msg --reparagraph -r ${PREV_REV}:HEAD -o dist/escape-${VERSION}-${REV}/ChangeLog.txt
+(cd dist && zip -r -9 escape-${VERSION}-${REV}.zip escape-${VERSION}-${REV}/*)
 
