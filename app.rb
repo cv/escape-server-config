c3048d50a0322c91d02e0a59db6b7cc1fce1aa0d
chris.read <chris.read@59ab4f38-e3ec-11dd-a8eb-ddf07f3c1838>
2009-07-29 17:12:23 +0000

    All config is now loaded from ~/.escape/config. A basic config file will be created if it does not exist...

--- app.rb
+++ app.rb
@@ -23,26 +23,43 @@ require 'ramaze'
 # I got the workaround from http://markmail.org/message/rn6hmt6nguzopypk?q=%22undefined+method+%60exclusive%27+for+Thread:Class%22
 require 'thread'
 
+require 'yaml'
+require 'etc'
+
 #
 # Configuration Area Start
 #
 
-# $connectionString: This sets the connection string to use for your database. 
+home = Etc.getpwuid.dir 
+
+# Configuration is loaded from ~/.escape/config
+# If the file is not there we'll simply create it and make SQLite the db
+
+begin
+    cfg = YAML.load_file("#{home}/.escape/config")
+rescue
+    # File is not there, let's create a default!
+    FileUtils.makedirs("#{home}/.escape")
+    cfg = Hash.new
+    cfg["database"] = "sqlite:///#{File.expand_path(File.dirname(__FILE__))}/escape.db"
+    cfg["port"] = 7000
+    cfg["jdbc.url"] = "jdbc:mysql://localhost/escape?user=escape&password=escape"
+    f = File.new("#{home}/.escape/config", 'w')
+    f.write(YAML.dump(cfg))
+    f.close()
+end
 
 # First check if we're running under JRuby. If we are, load the JDBC connection string from a file
 begin
     include Java
     import java.lang.System
-    home = System.getProperty("user.home")
-
-    require 'yaml'
-    cfg = YAML.load_file(home + "/.escape/config")
     $connectionString = cfg["jdbc.url"]
 rescue
     # We're not in JRuby
+    $connectionString = cfg["database"]
     # 
     # SQLite
-    $connectionString = "sqlite:///#{File.expand_path(File.dirname(__FILE__))}/escape.db"
+    #$connectionString = "sqlite:///#{File.expand_path(File.dirname(__FILE__))}/escape.db"
     # MySQL
     #$connectionString = "mysql://escape:escape@localhost/escape"
     # Postgres
@@ -52,15 +69,8 @@ rescue
     # MySQL over JDBC for JRuby
     #$connectionString = "jdbc:mysql://localhost/escape?user=escape&password=escape"
 
+    $listenPort = cfg["port"]
 end
-
-# $listenPort: The TCP port we need to listen on to service requests
-#
-$listenPort = 7000
-
-# $accessLog: Path to the file where we need to log access to
-#
-#$accessLog = "access.log"
 
 #
 # Configuration Area End
@@ -70,7 +80,7 @@ $listenPort = 7000
 # any other working path
 $LOAD_PATH.unshift(__DIR__)
 
-EscapeVersion = 0.3
+EscapeVersion = 0.4
 
 # Initialize controllers and models
 require 'model/init'
