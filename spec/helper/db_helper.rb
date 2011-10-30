961192819ab7c78a28a7313c550684559e2a9a9e
Carlos Villela <cv@lixo.org>
2011-10-30 13:04:24 -0200

    reindent

--- spec/helper/db_helper.rb
+++ spec/helper/db_helper.rb
@@ -6,26 +6,26 @@ require 'ramaze/spec'
 require __DIR__('../../start')
 
 module DBHelper
-    def reset_db
-        App.create_table!
-        Environment.create_table!
-        Owner.create_table!
-        Key.create_table!
-        Value.create_table!
-        AppsEnvironments.create_table!
+  def reset_db
+    App.create_table!
+    Environment.create_table!
+    Owner.create_table!
+    Key.create_table!
+    Value.create_table!
+    AppsEnvironments.create_table!
 
-        if Environment[:name => 'default'].nil?
-            Environment.create(:name => 'default')
-        end
-
-        if Owner[:name => 'nobody'].nil?
-            nobody = Owner.create(:name => 'nobody', :email => 'nobody@nowhere.com', :password => 'nothing')
-            nobody.add_environment(Environment[:name => 'default'])
-        end
+    if Environment[:name => 'default'].nil?
+      Environment.create(:name => 'default')
     end
+
+    if Owner[:name => 'nobody'].nil?
+      nobody = Owner.create(:name => 'nobody', :email => 'nobody@nowhere.com', :password => 'nothing')
+      nobody.add_environment(Environment[:name => 'default'])
+    end
+  end
 
 end
 
 shared :db_helper do
-    extend DBHelper
+  extend DBHelper
 end
