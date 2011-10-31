aa8a9c8e2b6e6481ef1ea9cde6a8c4d312d840f0
Carlos Villela <cv@lixo.org>
2011-10-31 10:33:43 -0200

    make tests compatible with RSpec 1.x

--- spec/init.rb
+++ spec/init.rb
@@ -22,3 +22,14 @@ require 'ramaze/spec/bacon'
 
 require __DIR__('helper/db_helper')
 require __DIR__('../start')
+
+# compatibility between RSpec 1.x and Bacon
+
+def behaves_like(*whatever)
+  include Rack::Test::Methods
+  include DBHelper
+end
+
+def app
+  Ramaze.middleware
+end
